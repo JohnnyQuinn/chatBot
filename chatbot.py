d5e66416eb556990edb2a6a999a428c3ac123d68
johnnyquinn <jftq97@gmail.com>
2020-09-22 22:04:28 -0400

    [added] lists of games based on genre

---
+++ chatbot.py
@@ -0,0 +1,21 @@
+print("\nHello, I'm a chat bot for Johnny!\nYou can tell me a video game genre and I'll return a game Johnny plays in that genre!\nType 'done' to exit out")
+user_input = input("Name a video game genre: ")
+
+def get_bot_response(user_response) {
+    if "RPG" in user_response:
+        return choice(RPG)
+    if "shooter" or "FPS" in useresponse
+}
+#lists of games based on genre
+RPG = list["Fallout", "Skyrim", "Dishonored"]
+
+shooter = list["Arma", "Call Of Duty", "Halo", "Battlefield", "Valorant"]
+
+horror = list["Outlast", "Alien: Isolation"]
+
+strategy = list["Total War (my fav)", "XCOM", "Leage Of Legends"]
+
+cards = list["Poker, I absolutely love poker!", "Solitare"]
+
+arcade = list["Fall Guys", "Clone Drone in the Danger Zone"]
+
