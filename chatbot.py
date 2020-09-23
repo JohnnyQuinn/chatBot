a0184aa5e6b24cb3e13a5fd8dd9a73b2171ed1e0
johnnyquinn <jftq97@gmail.com>
2020-09-22 22:32:59 -0400

    [added] get_bot_response and while loop

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,29 @@
-print("\nHello, I'm a chat bot for Johnny!\nYou can tell me a video game genre and I'll return a game Johnny plays in that genre!\nType 'done' to exit out")
+import random
+
+print("\nHello, I\'m a chat bot for Johnny!\nYou can tell me a video game genre and I\'ll return a game Johnny plays in that genre!\nType \'done\' to exit out")
 user_input = input("Name a video game genre: ")
 
-def get_bot_response(user_response) {
-    if "RPG" in user_response:
-        return choice(RPG)
-    if "shooter" or "FPS" in useresponse
-}
 #lists of games based on genre
-RPG = list["Fallout", "Skyrim", "Dishonored"]
+RPG = ["Fallout", "Skyrim", "Dishonored"]
 
-shooter = list["Arma", "Call Of Duty", "Halo", "Battlefield", "Valorant"]
+shooter = ["Arma", "Call Of Duty", "Halo", "Battlefield", "Valorant"]
 
-horror = list["Outlast", "Alien: Isolation"]
+horror = ["Outlast", "Alien: Isolation"]
 
-strategy = list["Total War (my fav)", "XCOM", "Leage Of Legends"]
+strategy = ["Total War (my fav)", "XCOM", "Leage Of Legends"]
 
-cards = list["Poker, I absolutely love poker!", "Solitare"]
+cards = ["Poker, I absolutely love poker!", "Solitare"]
 
-arcade = list["Fall Guys", "Clone Drone in the Danger Zone"]
+arcade = ["Fall Guys", "Clone Drone in the Danger Zone"]
 
+def get_bot_response(user_response):
+    if "RPG" in user_response:
+        rpg_Response = random.choice(RPG)
+        return rpg_Response
+    if "shooter" or "FPS" in user_response:
+        return random.choice(shooter)
+
+while user_input != "done":
+    print(get_bot_response(user_input))
+    user_input = input("Name a video game genre: ")
+    
