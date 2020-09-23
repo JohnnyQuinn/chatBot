874207f2a4a7777c1a697046fa4bd7dea02a5875
johnnyquinn <jftq97@gmail.com>
2020-09-23 00:17:51 -0400

    [added] comments and polished up the text formatting that is printed in the terminal

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,33 @@
 import random
 
+#intro text and intial request for user input
 print("\nHello, I\'m a chat bot for Johnny!\nYou can tell me a video game genre and I\'ll tell you a game Johnny plays in that genre!\nType \'done\' to exit out")
 user_input = input("Name a video game genre: \n")
 
+#global variable to check if user response matches any lists
 plays_Game = False
 
 #lists of games based on genre
-RPG = ["Fallout", "Skyrim", "Dishonored"]
+RPG = ["Fallout", "Skyrim, one of his favorites", "Dishonored"]
 
-shooter = ["Arma", "Call Of Duty", "Halo", "Battlefield", "Valorant"]
+shooter = ["Arma", "Call Of Duty", "Halo, one of his favorites", "Battlefield", "Valorant"]
 
-horror = ["Outlast", "Alien: Isolation"]
+horror = ["Outlast", "Alien: Isolation, one of his favorites"]
 
-strategy = ["Total War (my fav)", "XCOM", "Leage Of Legends"]
+strategy = ["Total War, one of his favorites ", "XCOM", "Leage Of Legends","Chess"]
 
-cards = ["Poker, I absolutely love poker!", "Solitare"]
+cards = ["Poker, one of his favorites", "Solitare"]
 
-arcade = ["Fall Guys", "Clone Drone in the Danger Zone"]
+arcade = ["Fall Guys", "Clone Drone in the Danger Zone, one of his favorites"]
 
+#receives user input, checks to see if input matches any genres, and returns response based on input
 def get_bot_response(user_response):
     global plays_Game
-    if user_response == "RPG":
+    user_response = user_response.lower()
+    if user_response == "rpg":
         plays_Game = True
         return  random.choice(RPG)
-    elif "shooter" == user_response or "FPS" == user_response:
+    elif "shooter" == user_response or "fps" == user_response:
         plays_Game = True
         return random.choice(shooter)
     elif "horror" == user_response:
@@ -39,13 +43,14 @@ def get_bot_response(user_response):
         plays_Game = True
         return  random.choice(arcade)
     elif plays_Game is False:
-        print(f"Johnny doesn't play {user_response}")
+        print(f"\n* Johnny doesn't play {user_response} *\n")
 
-while user_input != "done":
+#loop to repeatedly ask user for input and prints response from get_bot_response
+while user_input.lower() != "done":
     game = get_bot_response(user_input)
     if plays_Game is True:
-        print(f"Johnny plays {game}")
+        print(f"\n* Johnny plays {game} *\n")
         plays_Game = False
-    user_input = input("Name a video game genre: \n")
+    user_input = input("Type \'done\' to exit out\nName a video game genre: \n")
 
     
